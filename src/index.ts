430aa97d4c811437807834c65943c758c6a5baa1
Roberto Junior <robertojuniordev@gmail.com>
2023-10-06 17:10:05 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,4 @@
-// import 'dotenv/config';
-// import { updateThumb } from "./update-thumb";
+import 'dotenv/config';
+import { updateThumb } from "./update-thumb";
 
-import { renderHTML } from "./utils/render-html";
-import path from 'node:path';
-
-// updateThumb.execute();
-
-const generateImageFromHTML = require('./utils/gen-image.js');
-
-generateImageFromHTML(
-  renderHTML({ comment: '', imageURL: '', name: '' }),
-  path.resolve(__dirname, '..', 'image.png')
-).then(console.log)
-.catch(console.error);
+updateThumb.execute();
